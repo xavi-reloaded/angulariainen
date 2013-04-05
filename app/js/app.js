9dfb9954a50af35f3056c4239c083643a4dee215
xavi <00jabba>
2013-04-05 21:23:21 +0200

    first commit (still)

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('app', ['app.filters', 'app.services', 'app.directives','ui.bootstrap','ui.bootstrap-apium']).
+angular.module('app', ['app.filters', 'app.services', 'app.directives','ui.bootstrap']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
